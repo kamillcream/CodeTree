2b63770385f64d899cd027fdaf6ea5d439ae5f17
Jinyoung Park <glitt5384@naver.com>
2025-05-01 19:19:39 +0900

    Title: 최고의 33위치, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250501/최고의 33위치/best-place-of-33.py
+++ 250501/최고의 33위치/best-place-of-33.py
@@ -4,10 +4,10 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 def get_num_of_gold(row_s, col_s): # row_s + 2, col_s + 2
     gold_count = 0
-    for i in (row_s, row_s + 2):
-        for j in (col_s, col_s + 2):
+    for i in range(row_s, row_s + 3):
+        for j in range(col_s, col_s + 3):
             if grid[i][j] == 1:
-                gold_count += grid[i][j]
+                gold_count += 1
     return gold_count
 
 max_gold = 0
